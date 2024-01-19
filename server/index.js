b71a932d04cd542700e60bd460fe14e96e0fce51
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-01-20 00:25:05 +0530

    cors update to handle requests

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin :'http://localhost:3001',
+    methods : ['GET','POST','PUT','DELETE'],
+    credentials : true
+}));
+
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(cookieParser())
